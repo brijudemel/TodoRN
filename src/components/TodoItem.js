51b4796a4f7f6dabc1e60be51159d9f86b896cb5
brijudemel <brijudemel@gmail.com>
2021-07-17 00:09:15 +0530

    before using icons

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {View, Text, TextInput, Button} from 'react-native';
+import {View, Text, Button} from 'react-native';
 const TodoItem = ({todo}) => {
   return (
-    <View>
-      <Text>{todo}</Text>
+    <View style={styles.card}>
+      <View style={styles.cardContent}>
+        <Text style={styles.todo}>{todo}</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 6,
+    elevation: 5,
+    backgroundColor: '#fff',
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: '#333',
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    marginHorizontal: 4,
+    marginVertical: 6,
+  },
+  cardContent: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  todo: {
+    fontSize: 25,
+  },
+});
 
 export default TodoItem;
